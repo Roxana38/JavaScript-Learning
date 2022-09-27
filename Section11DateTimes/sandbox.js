7c9dd8c5e203e37552da2e4d63d1223b7f9b7090
Roxana38 <98492894+Roxana38@users.noreply.github.com>
2022-09-27 11:32:41 +0300

    date & time practice

---
+++ Section11DateTimes/sandbox.js
@@ -0,0 +1,55 @@
+//82.Date & Times in JavaScript
+
+/*const now = new Date();
+
+console.log(now);
+console.log(typeof now);
+
+// year, months , day , times
+console.log('getFullYear:',now.getFullYear());
+console.log('getMonth:',now.getMonth());
+console.log('getDate:',now.getDate());
+console.log('getDay:',now.getDay());
+console.log('getHours:',now.getHours());
+console.log('getMinutes:', now.getMinutes());
+console.log('getSeconds:', now.getSeconds());
+
+//timestamps
+
+console.log('timestamp:', now.getTime());
+
+// date strings
+console.log(now.toDateString());
+console.log(now.toTimeString());
+console.log(now.toLocaleString());*/
+
+//83. Timestamps & Comparations
+/*
+//const before = new Date('02/01/2019 7:30:59');
+const before = new Date('February 1 2019 7:30:59');
+const now = new Date();
+
+//console.log(now.getTime(),before.getTime());
+
+const diff = now.getTime() - before.getTime();
+console.log(diff);
+
+const mins = Math.round(diff / 1000 / 60);
+//console.log(mins);
+
+const hours = Math.round(mins / 60);
+//console.log(hours);
+
+const days = Math.round(hours / 24);
+console.log(mins,hours,days);
+
+console.log(`the before date was ${mins} mins ago`);
+console.log(`the before date was ${hours} hours ago`);
+console.log(`the before date was ${days} days ago`);
+
+
+// converting timestamps to dates
+const timestamp = 1675938474990;
+console.log(new Date(timestamp))*/
+
+//.84 Builing a digital Clock
