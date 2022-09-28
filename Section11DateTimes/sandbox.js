0ba7969976a42d7800d56c684ce99f3d648f6578
Roxana38 <98492894+Roxana38@users.noreply.github.com>
2022-09-28 06:28:51 +0300

    buiding a digital clock

--- Section11DateTimes/sandbox.js
+++ Section11DateTimes/sandbox.js
@@ -53,3 +53,23 @@ const timestamp = 1675938474990;
 console.log(new Date(timestamp))*/
 
 //.84 Builing a digital Clock
+
+/*const clock = document.querySelector('.clock');
+
+const tick = () => {
+    const now = new Date();
+    
+    const h = now.getHours();
+    const m = now.getMinutes();
+    const s = now.getSeconds();
+
+    const html=`
+    <span>${h}</span> :
+    <span>${m}</span> :
+    <span>${s}</span>
+    `;
+    clock.innerHTML = html;
+};
+
+setInterval(tick, 1000);*/
+
