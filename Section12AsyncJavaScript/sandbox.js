563db84ee211bedfae8b55d3df1831f7e96de256
Roxana38 <98492894+Roxana38@users.noreply.github.com>
2022-09-29 06:06:29 +0300

    json data

--- Section12AsyncJavaScript/sandbox.js
+++ Section12AsyncJavaScript/sandbox.js
@@ -42,3 +42,72 @@ request.addEventListener('readystatechange', () => {
 
 request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
 request.send();*/
+
+//91.Callback Function
+
+/*const getTodos = (callback) => {
+
+    const request = new XMLHttpRequest();
+  
+    request.addEventListener('readystatechange', () => {
+  
+      if(request.readyState === 4 && request.status === 200){
+        callback(undefined, request.responseText);
+      } else if (request.readyState === 4){
+        callback('could not fetch the data', undefined);
+      }
+  
+    });
+    
+    request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+    request.send();
+  
+  };
+  
+  console.log(1);
+  console.log(2);
+  
+  getTodos((err, data) => {
+    console.log('callback function fired');
+    if(err){
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });
+  
+  console.log(3);
+  console.log(4);*/
+
+
+  //92.JSON Data
+
+ /* const getTodos = (callback) => {
+
+    const request = new XMLHttpRequest();
+  
+    request.addEventListener('readystatechange', () => {
+  
+      if(request.readyState === 4 && request.status === 200){
+        const data = JSON.parse(request.responseText);
+        callback(undefined, data);
+      } else if (request.readyState === 4){
+        callback('could not fetch the data', undefined);
+      }
+  
+    });
+    
+    request.open('GET', 'todos.json');
+    request.send();
+  
+  };
+  
+  getTodos((err, data) => {
+    console.log('callback function fired');
+    if(err){
+      console.log(err);
+    } else {
+      console.log(data);
+    }
+  });*/
+
