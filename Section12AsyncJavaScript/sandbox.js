1cc17a06cccaf3c56fe60ad7c8940f55966ef3db
Roxana38 <98492894+Roxana38@users.noreply.github.com>
2022-09-29 06:12:13 +0300

    chaining promises

--- Section12AsyncJavaScript/sandbox.js
+++ Section12AsyncJavaScript/sandbox.js
@@ -110,4 +110,123 @@ request.send();*/
       console.log(data);
     }
   });*/
+// 93.Callback Hell
+/*const getTodos = (resource, callback) => {
 
+    const request = new XMLHttpRequest();
+  
+    request.addEventListener('readystatechange', () => {
+  
+      if(request.readyState === 4 && request.status === 200){
+        const data = JSON.parse(request.responseText);
+        callback(undefined, data);
+      } else if (request.readyState === 4){
+        callback('could not fetch the data', undefined);
+      }
+  
+    });
+    
+    request.open('GET', resource);
+    request.send();
+  
+  };
+  
+  getTodos('json/luigi.json', (err, data) => {
+    console.log(data);
+    getTodos('json/mario.json', (err, data) => {
+      console.log(data);
+      getTodos('json/shaun.json', (err, data) => {
+        console.log(data);
+      });
+    });
+  });*/
+
+
+  //94.Promise Basics
+
+  /*const getTodos = (resource) => {
+
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+  
+      request.addEventListener('readystatechange', () => {
+    
+        if(request.readyState === 4 && request.status === 200){
+          const data = JSON.parse(request.responseText);
+          resolve(data);
+        } else if (request.readyState === 4){
+          reject('could not fetch the data');
+        }
+    
+      });
+      
+      request.open('GET', resource);
+      request.send();
+    });
+  
+  };
+  
+  getTodos('json/luigi.json').then(data => {
+    console.log('promise resolved:', data);
+  }).catch(err => {
+    console.log('promise rejected:', err);
+  });
+  
+  // promise example
+  const getSomething = () => {
+  
+    return new Promise((resolve, reject) => {
+      // do something (fetch data)
+      // resolve('some data');
+      reject('some error');
+    });
+  
+  };
+  */
+  // getSomething().then(data => {
+  //   console.log('promise resolved:', data);
+  // }, err => {
+  //   console.log('promise rejected:', err);
+  // });
+  
+  // getSomething().then(data => {
+  //   console.log('promise resolved:', data);
+  // }).catch(err => {
+  //   console.log('promise rejected:', err);
+  // });
+
+  //95.Chaining promises
+
+  /*const getTodos = (resource) => {
+
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+  
+      if(request.readyState === 4 && request.status === 200){
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else if (request.readyState === 4){
+        reject('could not fetch the data');
+      }
+  
+    });
+    
+    request.open('GET', resource);
+    request.send();
+  });
+
+};
+
+getTodos('json/luigi.json').then(data => {
+  console.log('promise 1 resolved:', data);
+  return getTodos('json/mario.json');
+}).then(data => {
+  console.log('promise 2 resolved:', data);
+  return getTodos('json/shaun.json');
+}).then(data => {
+  console.log('promise 3 resolved:', data);
+}).catch(err => {
+  console.log('promise rejected:', err);
+});*/
